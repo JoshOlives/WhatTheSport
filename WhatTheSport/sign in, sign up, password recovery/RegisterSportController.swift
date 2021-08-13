dfc261294a15a4248f68a7a1f220da1a5bf09623
amgluch <38114837+amgluch@users.noreply.github.com>
2021-08-13 05:11:32 -0500

    pre-selection, and alert when 0 sport select

--- WhatTheSport/sign in, sign up, password recovery/RegisterSportController.swift
+++ WhatTheSport/sign in, sign up, password recovery/RegisterSportController.swift
@@ -34,30 +34,49 @@ class RegisterSportController: UIViewController {
         for view in sportViewArray {
             view.addTarget(self, action: #selector(selectSport(_:)), for: .touchDown)
         }
+        
+        let userSports = fireUser?.get("sports") as! [String]
+        if userSports.count != 0 {
+            selectedSports = userSports
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
         let background: UIColor = currentUser!.settings!.dark ? .black : UIColor(rgb: Constants.Colors.lightOrange)
         let navImage: UIImage? = currentUser!.settings!.dark ? UIImage() : nil
+        
         self.navigationController?.navigationBar.setBackgroundImage(navImage, for: .default)
         view.backgroundColor = background
+        
         inTransition = false
-        for view in sportViewArray {
-            let background: UIColor = currentUser!.settings!.dark ? .darkGray : .white
-            view.backgroundColor = background
-        }
-        for label in labelCollection {
+        
+        for index in 0...3 {
+            let label = labelCollection[index]
             let textColor: UIColor = currentUser!.settings!.dark ? .white : .black
             label.textColor = textColor
+            let view = sportViewArray[index]
+            
+            if !selectedSports.contains(label.text!) {
+                let background: UIColor = currentUser!.settings!.dark ? .darkGray : .white
+                view.backgroundColor = background
+            } else { //pre-select a sport the user already follows
+                view.backgroundColor = UIColor(rgb: Constants.Colors.orange)
+            }
         }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "RegisterTeamIdentifier",
-            let destination = segue.destination as? RegisterTeamController {
-            destination.delegate = self
-            destination.selectedSports = self.selectedSports
+        if selectedSports.count != 0 {
+            if segue.identifier == "RegisterTeamIdentifier",
+                let destination = segue.destination as? RegisterTeamController {
+                destination.delegate = self
+                destination.selectedSports = self.selectedSports
+            }
+        } else {
+            let alertController = UI.createAlert(title: "No sports selected", msg: "Please select at least 1 sport.")
+            present(alertController, animated: true, completion: nil)
         }
     }
     
