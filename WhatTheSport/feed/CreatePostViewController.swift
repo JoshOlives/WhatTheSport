e9de9da375ce9a38f71f6b587dc5f1a615fdc42a
Joshua Olivares <joshuaolivares@utexas.edu>
2021-08-13 21:59:08 -0500

    team selector

--- WhatTheSport/feed/CreatePostViewController.swift
+++ WhatTheSport/feed/CreatePostViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 import FirebaseFirestore
 
 class CreatePostViewController: UIViewController {
+
+    
+    private var sport: String?
+    private var team: String?
+    private var button: UIButton!
     private var postTextView: UITextView!
     private var postBarButton: UIBarButtonItem!
     private var sportSelector: UIPickerView!
@@ -22,20 +27,29 @@ class CreatePostViewController: UIViewController {
 //                                        CGRect(x: 0, y: 0, width: safeArea.width, height: safeArea.height / 2))
         self.postTextView.translatesAutoresizingMaskIntoConstraints = false
         
-        self.sportSelector = UIPickerView(frame: .zero)
-        self.sportSelector.translatesAutoresizingMaskIntoConstraints = false
+        self.button = Button(title: "Choose team")
+        button.addTarget(self, action: #selector(selectTeam), for: .touchUpInside)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        constraints.append(button.centerXAnchor.constraint(equalTo: self.postTextView.centerXAnchor))
+        constraints.append(button.topAnchor.constraint(equalTo: self.postTextView.bottomAnchor))
+        constraints.append(button.widthAnchor.constraint(equalToConstant: view.bounds.width-50))
+        constraints.append(button.heightAnchor.constraint(equalToConstant: Constants.Field.height))
+        
+        //self.sportSelector.translatesAutoresizingMaskIntoConstraints = false
         
         self.view.addSubview(postTextView)
-        self.view.addSubview(sportSelector)
+        self.postTextView.addSubview(sportSelector)
         let safe = self.view.safeAreaLayoutGuide
         
         constraints.append(self.postTextView.topAnchor.constraint(equalTo: safe.topAnchor))
         constraints.append(self.postTextView.widthAnchor.constraint(equalTo: safe.widthAnchor))
         constraints.append(self.postTextView.heightAnchor.constraint(equalToConstant: self.view.bounds.height / 2 - 50))
         
-        constraints.append(self.sportSelector.topAnchor.constraint(equalTo: self.postTextView.bottomAnchor))
-        constraints.append(self.sportSelector.widthAnchor.constraint(equalToConstant: self.view.bounds.width / 4))
-        constraints.append(self.sportSelector.heightAnchor.constraint(equalToConstant: 50))
+       // constraints.append(self.sportSelector.topAnchor.constraint(equalTo: self.postTextView.bottomAnchor))
+       // constraints.append(self.sportSelector.widthAnchor.constraint(equalToConstant: self.view.bounds.width / 4))
+       // constraints.append(self.sportSelector.heightAnchor.constraint(equalToConstant: 50))
+        self.view.addSubview(button)
         
         self.postTextView.font = .systemFont(ofSize: 18)
         
@@ -44,6 +58,8 @@ class CreatePostViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.sportSelector = UIPickerView(frame: CGRect(x: 100, y: 100, width: 1000, height: 1000))
+        
         self.title = "Create Post"
         self.postBarButton = UIBarButtonItem(title: "Post", style: .plain, target: self, action: #selector(createPost))
         self.postBarButton.tintColor = UIColor.white
@@ -52,6 +68,11 @@ class CreatePostViewController: UIViewController {
         self.postTextView = UITextView(frame: .zero)
 
         
+    }
+    
+    @objc
+    func selectTeam() {
+        print("select team")
     }
     
     override func viewWillAppear(_ animated: Bool) {
